5e7c179cd843e141d6d61e0bd4215c08ecff9783
G++ <burpsuite@gmail>
2022-11-16 22:37:21 +0800

    update 2000

--- C course/2000.cpp
+++ C course/2000.cpp
@@ -1,13 +1,11 @@
-#include <iostream>
 #include <algorithm>
-int main()
-{
+#include <iostream>
+#include <iomanip>
+int main() {
 	int a[10], sum = 0;
 	for (int i = 0; i < 10; i++) {
 		std::cin >> a[i];
 		sum = sum + a[i];
 	}
-	std::sort(a, a + 10);
-	printf("%d %d %.2f", a[9], a[0], (float)sum / 10);
-	return 0;
+	std::cout << *std::max_element(a, a + 10) << " " << *std::min_element(a, a + 10) << " " << std::fixed << std::setprecision(2) << (float)sum / 10;
 }
