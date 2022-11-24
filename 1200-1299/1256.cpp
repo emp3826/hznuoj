963ed3ba30cc8d986f251fcbd9ffe94be8885b05
emp3826 <30045531+emp3826@users.noreply.github.com>
2022-11-24 12:51:39 +0800

    Update 1256.cpp

--- 1200-1299/1256.cpp
+++ 1200-1299/1256.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 #include <map>
 #include <sstream>
-#include <string>
-std::map<std::string,int>a;
+std::map<std::string, int>m;
 int main() {
-    std::string s,buf;
-    int n,flag=0;
-    while (std::cin>>n) {
+    std::string s, b;
+    int n, flag = 0;
+    while (std::cin >> n) {
         if (flag)
             puts("");
         flag++;
-        a.clear();
-        while (std::getline(std::cin,s)&&s!="EndOfText") {
-            for(int i=0; i<s.length(); i++)
-                if (isalpha(s[i])) s[i]=tolower(s[i]);
-                else s[i]=' ';
+        m.clear();
+        while (std::getline(std::cin, s) && s != "EndOfText") {
+            for (int i = 0; i < s.length(); i++)
+                if (isalpha(s[i])) s[i] = tolower(s[i]);
+                else s[i] = ' ';
             std::stringstream ss(s);
-            while (ss>>buf) a[buf]++;
+            while (ss >> b) m[b]++;
         }
-        int tt=0;
-        std::map<std::string,int>::iterator it;
-        for(it=a.begin(); it!=a.end(); it++)
-            if(it->second==n) {
-                tt=1;
-                std::cout<<it->first<<std::endl;
+        int t = 0;
+        std::map<std::string, int>::iterator it;
+        for (it = m.begin(); it != m.end(); it++)
+            if (it->second == n) {
+                t = 1;
+                std::cout << it->first << std::endl;
             }
-        if(!tt) std::cout << "There is no such word." <<std::endl;
+        if (!t) std::cout << "There is no such word.\n";
     }
 }
