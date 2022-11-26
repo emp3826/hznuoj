5a19220c2b3ef0d513d3a436001a5128bd352cea
G++ <burpsuite@gmail>
2022-11-26 13:19:09 +0800

    update chore

--- C course/1941.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-	float m, n;
-	scanf("%f%f", &m, &n);
-	printf("%.2f\n", m + n);
-	printf("%.2f\n", m - n);
-	printf("%.2f\n", (m + n) / 2);
-}
--- C course/1971.c
+++ C course/1971.c
@@ -1,45 +1,14 @@
-#include<stdio.h>
-#include<string.h>
-#include<math.h>
-#define MAXV 1000011
-int isPrime[MAXV + 1];
-int prime[MAXV + 1];
-int size = 0;
-void getprime();
-int main()
-{
-    int t;
-    scanf("%d", &t);
-    getprime();
-    while (t--)
-    {
-        int n;
-        scanf("%d", &n);
-        if (isPrime[n])
-        {
-            printf("YES\n");
-        }
-        else
-        {
-            printf("NO\n");
-        }
-    }
-    return 0;
-}
-
-void getprime() {
-    for (int i = 2; i < MAXV; i++)
-    {
-        isPrime[i] = 1;
-    }
-    for (int i = 2; i < MAXV; i++)
-    {
-        if (isPrime[i])
-        {
-            for (int j = 2; i * j < MAXV; j++)
-            {
-                isPrime[i * j] = 0;
-            }
-        }
-    }
+#include <stdio.h>
+int np[1000001];
+int main() {
+	for (int i = 2; i * i <= 1000000; i++)
+		for (int t = 2; i * t <= 1000000; t++)
+			np[t * i] = 1;
+	np[1] = 1;
+	int t, n;
+	scanf("%d", &t);
+	while (t--) {
+		scanf("%d", &n);
+		(np[n]) ? printf("NO\n") : printf("YES\n");
+	}
 }
