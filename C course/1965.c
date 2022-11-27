c4b20cd15754fc730090234623a8dd576802e77c
G++ <burpsuite@gmail>
2022-11-27 18:06:41 +0800

    update

--- C course/1965.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-	int i;
-	int sum = 0;
-	for (i = 2; i <= 100; i = i + 2) {
-		sum = sum + i;
-	}
-	printf("%d\n", sum);
-}
---
+++ C course/1967.py
@@ -0,0 +1,1 @@
+print('\n'.join([' '.join(['%s*%s=%s' % (i,j,i*j) for j in range(1,i+1)]) for i in range(1,10)]))
